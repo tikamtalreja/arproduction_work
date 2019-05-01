c6479b044763456a6382521aa9b6f265a2e59b74
Tikam Talreja <tikamtalreja@localhost.localdomain>
2019-05-01 14:42:14 +0530

    edit update error

--- lib/db/PostService.dart
+++ lib/db/PostService.dart
@@ -20,7 +20,8 @@ class PostService{
 
   updatePost(){
     database.reference().child('$nodeName/${post['key']}').update(
-      {"title": post['title'], "body": post['body'], "date":post['date']}
+      {"name": post['name'], "address": post['address'], "date":post['date'],"time":post['time'],"phone1":post['phone1'],
+      "phone2":post['phone2'],"colb":post['colb'],"desc":post['desc'],"venue":post['venue']}
     );
   }
 }
